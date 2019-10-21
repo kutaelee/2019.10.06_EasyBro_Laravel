bb5f7a868af13067c6e2fc84e08281a35dbdf649
kutaelee <kutaelee0@gmail.com>
2019-10-21 16:20:03 +0900

    이메일 내용 변경

--- resources/views/authMail.blade.php
+++ resources/views/authMail.blade.php
@@ -2,6 +2,6 @@
     <body>
         <h2>EASY BRO에서 비밀번호 변경 인증메일보내드립니다.</h2>
     <a style="font-size:25px"href="http://127.0.0.1:8000/auth?key={{ $key }}">인증을 진행하시려면 클릭해주세요.</a><br/><br/>
-    관리자 이메일 : kutaelee0@gmail.com<br/>
+    본인이 보내신게 아니시거나 문의사항이 있다면 답장을 보내시면 처리해드리겠습니다.<br/>
     </body>
 </html>
